9a4e552b603b96b11f903bef0e9382d92cdc45fe
Artem <artem@webart-tech.ru>
2017-07-17 09:42:04 +0700

    Adding STATIC_ROOT

--- sm_spy/sm_spy/settings.py
+++ sm_spy/sm_spy/settings.py
@@ -19,7 +19,7 @@ from celery.schedules import crontab
 djcelery.setup_loader()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
