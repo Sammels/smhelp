e92c7605342a2a16620b5a9de818a5127757785a
Artem <artem@webart-tech.ru>
2017-08-08 20:10:05 +0700

    Fixing countries

--- sm_spy/vk_app/management/commands/vk_country_init.py
+++ sm_spy/vk_app/management/commands/vk_country_init.py
@@ -13,9 +13,33 @@ class Command(BaseCommand):
         trying = 1
         while len(data) == 1000:
             for d in data:
-                Country.objects.get_or_create(id=d['cid'], name=d['title'])
+                counry, create  = Country.objects.get_or_create(id=d['cid'], name=d['title'])
+                data_cities = api.database.getCities(country_id=counry.pk, count=1000, need_all=1)
+                ctrying = 1
+                while len(data_cities) == 1000:
+                    for data_city in data_cities:
+                        City.objects.get_or_create(country_id=counry.pk, name=data_city['title'], id=data_city['cid'])
+                    if len(data_cities) == 1000:
+                        data_cities = api.database.getCities(country_id=d['cid'], count=1000, need_all=1,
+                                                             offset=1000 * ctrying)
+                        ctrying += 1
+
+                for data_city in data_cities:
+                    City.objects.get_or_create(country_id=counry.pk, name=data_city['title'], id=data_city['cid'])
             if len(data) == 1000:
                 data = api.database.getCountries(count=1000, need_all=1, offset=1000*trying)
                 trying += 1
         for d in data:
-            Country.objects.get_or_create(id=d['cid'], name=d['title'])
+            counry, create = Country.objects.get_or_create(id=d['cid'], name=d['title'])
+            data_cities = api.database.getCities(country_id=counry.pk, count=1000, need_all=1)
+            ctrying = 1
+            while len(data_cities) == 1000:
+                for data_city in data_cities:
+                    City.objects.get_or_create(country_id=counry.pk, name=data_city['title'], id=data_city['cid'])
+                if len(data_cities) == 1000:
+                    data_cities = api.database.getCities(country_id=d['cid'], count=1000, need_all=1,
+                                                         offset=1000 * ctrying)
+                    ctrying += 1
+
+            for data_city in data_cities:
+                City.objects.get_or_create(country_id=counry.pk, name=data_city['title'], id=data_city['cid'])
